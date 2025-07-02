0ca4a9416b5cbdcfbc4cdbc7e3997edcc9affe2c
Finger-cori <badzoo@naver.com>
2025-07-02 14:52:23 +0900

    .

--- APP/main.py
+++ APP/main.py
@@ -20,11 +20,12 @@ def main(args):
     vad_pipe = VADPipe(vad_config)
     diar_pipe = DIARPipe(diar_config)
     postprocess_pipe = PostProcessPipe(db_config)
-    
+    start = time.time()     
     '''
     Cleanse audio, Get VAD Result, Get Diar Result, Process Diar Result 
     '''
     clean_audio = frontend_pipe.process_audio(args.file_name, chunk_length=args.chunk_length, deverve=True)
+    print(f'audio cleanse time: {time.time() - start}초')
     vad_result = vad_pipe.get_vad_timestamp(clean_audio)
     diar_result, _ = diar_pipe.get_diar(args.file_name, return_embeddings=False)   # emb 값 사용 x 
     processed_diar, non_overlapped_diar = diar_pipe.preprocess_result(diar_result=diar_result, vad_result=vad_result)    # ok. 
@@ -35,7 +36,7 @@ def main(args):
     full_diar = postprocess_pipe.apply_labels_to_full_diar(processed_diar, non_overlapped_diar)
     final_diar = postprocess_pipe.apply_label_mapping_to_diar(full_diar, label_mapping_dict)
     diar_pipe.save_merged_rttm(final_diar, file_name=args.file_name)
-    
+    print(f'total time: {time.time() - start}초')
 
 if __name__ == '__main__':
     cli_parser = argparse.ArgumentParser()
