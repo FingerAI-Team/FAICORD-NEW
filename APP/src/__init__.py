4451cf46b82947f2f56a2a077d38e6a7b95decc4
Finger-cori <badzoo@naver.com>
2025-07-08 15:32:31 +0900

    Update __init__.py
    add summary pipe

--- APP/src/__init__.py
+++ APP/src/__init__.py
@@ -3,4 +3,5 @@ from .audio_handler import VoiceEnhancer, NoiseHandler, AudioVisualizer
 from .pyannotes import PyannotVAD, PyannotDIAR, PyannotOSD
 from .embeddings import SBEMB, WSEMB, EMBVisualizer
 from .clusters import KNNCluster
-from .pipe import FrontendPipe, VADPipe, DIARPipe, PostProcessPipe, STTPipe
+from .llms import LLMOpenAI
+from .pipe import FrontendPipe, VADPipe, DIARPipe, STTPipe, SummaryPipe, PostProcessPipe
