41912e16b77e6c2ca8a3a8c3240eda7d49d4be4b
Finger-cori <badzoo@naver.com>
2025-07-10 17:27:46 +0900

    Update main.py
    summary pipeline add

--- APP/main.py
+++ APP/main.py
@@ -1,6 +1,7 @@
-from src import FrontendPipe, VADPipe, DIARPipe, PostProcessPipe, STTPipe
+from src import FrontendPipe, VADPipe, DIARPipe, PostProcessPipe, STTPipe, SummaryPipe
 from dotenv import load_dotenv
 import argparse
+import markdown
 import time
 import json
 import os
@@ -11,17 +12,18 @@ def main(args):
     Default Setting
     '''
     start = time.time()
+    load_dotenv()
+    with open(os.path.join(args.whisper_config_path, 'generation_config.json')) as f: 
+        generation_config = json.load(f)
+
     vad_config = os.path.join(args.model_config_path, 'pyannote_vad_config.yaml')
     diar_config = os.path.join(args.model_config_path, 'pyannote_diarization_config.yaml')
     frontend_pipe = FrontendPipe()
     vad_pipe = VADPipe(vad_config)
     diar_pipe = DIARPipe(diar_config)
     postprocess_pipe = PostProcessPipe()
-    
-    load_dotenv()
-    with open(os.path.join(args.whisper_config_path, 'generation_config.json')) as f: 
-        generation_config = json.load(f)
-    
+    summary_pipe = SummaryPipe(config=generation_config, api_key=os.getenv('OPENAI_API'))
+    openai_summary_model = summary_pipe.set_openai_client()
     rttm_file = args.file_name.replace('/audio/', '/rttm/').replace('.wav', '.rttm')
     whisper_api = os.getenv('OPENAI_API')
     stt_pipe = STTPipe(whisper_api=whisper_api, generation_config=generation_config)
@@ -35,21 +37,31 @@ def main(args):
     diar_result, _ = diar_pipe.get_diar(args.file_name, return_embeddings=False)   # emb 값 사용 x 
     processed_diar, non_overlapped_diar = diar_pipe.preprocess_result(diar_result=diar_result, vad_result=vad_result)    # ok. 
     # relabeled_diar = postprocess_pipe.relabel_nonoverlapped_labels(args.file_name, non_overlapped_diar)
+    
     chunk_emb_array = postprocess_pipe.get_chunk_emb_array(args.file_name, non_overlapped_diar)
     label_mapping_dict = postprocess_pipe.build_label_mapping_dict(chunk_emb_array)
     # print(label_mapping_dict)
+    
     full_diar = postprocess_pipe.apply_labels_to_full_diar(processed_diar, non_overlapped_diar)
     final_diar = postprocess_pipe.apply_label_mapping_to_diar(full_diar, label_mapping_dict)
     rttm_file = args.file_name.replace('/audio/', '/rttm/').replace('.wav', '.rttm')
     diar_pipe.save_merged_rttm(final_diar, file_name=rttm_file)
     print(f'Diarization Done !: {time.time() - start}초')
+    
     diar_result = stt_pipe.read_rttm(rttm_file)
     stt_result = stt_pipe.transcribe_by_rttm(args.file_name, diar_result)
+    print(f'STT Done !: {time.time() - start}초')
     save_file_name = 'faicord_' + args.file_name.split('/')[-1].split('.')[0] + '.json'
-    
     with open(os.path.join('./dataset/stt/', save_file_name), "w", encoding="utf-8") as f:
         json.dump(stt_result, f, ensure_ascii=False, indent=2)
-    print(f"total time: {time.time() - start}")
+   
+    summary_result = summary_pipe.summarize(openai_summary_model, stt_result) 
+    print(f'Summarize Done !: {time.time() - start}초')
+    markdown_text = summary_pipe.convert_minutes_to_markdown(summary_result)
+    save_file_name = 'faicord_' + args.file_name.split('/')[-1].split('.')[0] + '_summary.html'    
+    html_text = markdown.markdown(markdown_text, extensions=["fenced_code", "tables"])
+    with open(os.path.join('./dataset/summary/', save_file_name), "w", encoding="utf-8") as f:
+        f.write(html_text)
 
 if __name__ == '__main__':
     cli_parser = argparse.ArgumentParser()
