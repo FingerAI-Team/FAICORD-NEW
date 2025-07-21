db31aa0424ad421174bfa8c5c1b91cbf98a566e2
Finger-cori <badzoo@naver.com>
2025-07-21 13:43:00 +0900

    Update summary_api.py

--- APP/summary_api.py
+++ APP/summary_api.py
@@ -2,6 +2,7 @@ from markdown_pdf import Section
 from dotenv import load_dotenv
 from src import SummaryPipe
 import argparse
+import markdown
 import json
 import os
 
@@ -14,12 +15,19 @@ def main(args):
     with open(os.path.join(args.whisper_config_path, 'generation_config.json')) as f: 
         generation_config = json.load(f)
     
+    with open(os.path.join('./config', "default_system_prompt.txt"), "r", encoding="utf-8") as f:
+        system_prompt = f.read()
+    
+    with open(os.path.join('./config', "default_subrole_prompt.txt"), "r", encoding="utf-8") as f:
+        subrole_prompt = f.read()
+
     print(f'Generation Config: {generation_config}')
     summary_pipe = SummaryPipe(config=generation_config, api_key=os.getenv('OPENAI_API'))
     openai_summary_model = summary_pipe.set_openai_client()
     with open(args.file_name, 'r', encoding='utf-8') as f:
         stt_result = json.load(f)  
-    summary_result = summary_pipe.summarize(openai_summary_model, stt_result) 
+    # print(stt_result)
+    summary_result = summary_pipe.summarize(openai_summary_model, stt_result, system_prompt=system_prompt, subrole_prompt=subrole_prompt) 
     markdown_text = summary_pipe.convert_minutes_to_markdown(summary_result)
     save_file_name = 'faicord_' + args.file_name.split('/')[-1].split('.')[0] + '_summary.html'    
     html_text = markdown.markdown(markdown_text, extensions=["fenced_code", "tables"])
