b2025a9f090459976a6982b7c3509ac2be6fa016
Finger-cori <badzoo@naver.com>
2025-08-04 17:54:19 +0900

    Update summary_api.py

--- APP/summary_api.py
+++ APP/summary_api.py
@@ -18,7 +18,7 @@ def main(args):
     with open(os.path.join('./config', "default_system_prompt.txt"), "r", encoding="utf-8") as f:
         system_prompt = f.read()
     
-    with open(os.path.join('./config', "default_subrole_prompt.txt"), "r", encoding="utf-8") as f:
+    with open(os.path.join('./config', "default_subrole_div_prompt.txt"), "r", encoding="utf-8") as f:
         subrole_prompt = f.read()
 
     print(f'Generation Config: {generation_config}')
@@ -27,8 +27,19 @@ def main(args):
     with open(args.file_name, 'r', encoding='utf-8') as f:
         stt_result = json.load(f)  
     # print(stt_result)
-    summary_result = summary_pipe.summarize(openai_summary_model, stt_result, system_prompt=system_prompt, subrole_prompt=subrole_prompt) 
-    markdown_text = summary_pipe.convert_minutes_to_markdown(summary_result)
+
+    stt_results = summary_pipe.split_stt_result(stt_result, chunk_count=3)  # [초반부, 중반부, 후반부]
+    chunk_summary = '' 
+    for idx in range(len(stt_results)):
+        summary_result = summary_pipe.summarize(openai_summary_model, stt_results[idx], system_prompt=system_prompt, subrole_prompt=subrole_prompt) 
+        chunk_summary += summary_result + '\n\n'
+
+    with open(os.path.join('./config', "concat_system_prompt.txt"), "r", encoding="utf-8") as f:
+        system_prompt = f.read()
+
+    total_summary = summary_pipe.summarize(openai_summary_model, chunk_summary, system_prompt=system_prompt, subrole_prompt='')   
+    print(total_summary)
+    markdown_text = summary_pipe.convert_minutes_to_markdown(total_summary)
     save_file_name = 'faicord_' + args.file_name.split('/')[-1].split('.')[0] + '_summary.html'    
     html_text = markdown.markdown(markdown_text, extensions=["fenced_code", "tables"])
     with open(os.path.join('./dataset/summary/', save_file_name), "w", encoding="utf-8") as f:
