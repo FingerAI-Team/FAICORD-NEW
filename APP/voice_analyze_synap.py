a09e2f8877138483931a280890a0fd727a614abe
Finger-cori <badzoo@naver.com>
2025-12-26 09:57:45 +0900

    Update voice_analyze_synap.py
    folder name update

--- APP/voice_analyze_synap.py
+++ APP/voice_analyze_synap.py
@@ -9,13 +9,13 @@ def main(args):
     analyzer = VoiceAnalyzer(api_key=api_key)
     stt_pipe = STTPipe()
     ''' load rttm file '''
-    rttm_file = args.file_path.replace('/audio/', '/rttm/').replace('.wav', '.rttm')
+    rttm_file = args.file_path.replace('/audio/', '/diar_results/').replace('.wav', '.rttm')
     rttm_result = stt_pipe.read_rttm(rttm_file)
     ''' analyze with diar info '''
     processed_rttm = analyzer.process_rttm_for_stt(rttm_result)
     result = analyzer.analyze(file_path=args.file_path, input_type=args.input_type, timeline=processed_rttm)
     save_file_name = args.file_path.split('/')[-1].split('.')[0] + '_va.json'   # va: voice analyzer
-    analyzer.save_result_to_json(analysis_result=result, save_path=os.path.join('./dataset/stt/',save_file_name))
+    analyzer.save_result_to_json(analysis_result=result, save_path=os.path.join('./dataset/stt_results/',save_file_name))
 
 if __name__ == "__main__":
     cli_parser = argparse.ArgumentParser(description="Voice Analyzer Test")
