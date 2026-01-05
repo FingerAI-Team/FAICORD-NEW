99303f379b33ffa1dc8799c92f11883b894bad6b
Finger-cori <badzoo@naver.com>
2026-01-05 09:55:09 +0900

    Update preprocessors.py

--- APP/src/preprocessors.py
+++ APP/src/preprocessors.py
@@ -35,7 +35,8 @@ class DataProcessor:
 
     def calc_cer(self, context_stt, context_answer):
         cer_score = 0
-        # print(f'context stt: {context_stt}')
+        print(f'len context stt: {len(context_stt)}')
+        print(f'len context answer: {len(context_answer)}')
         for idx, _ in enumerate(context_stt):
             cer_score += cer(context_stt[idx]['text'], context_answer[idx]['text'])
         return cer_score / len(context_stt) if context_stt else 0
