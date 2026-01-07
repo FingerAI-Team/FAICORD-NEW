0d8e3bb12c887081e9ae0989435b032c99b74ded
Finger-cori <badzoo@naver.com>
2026-01-07 16:06:40 +0900

    Update voice_analyze_synap.py

--- APP/voice_analyze_synap.py
+++ APP/voice_analyze_synap.py
@@ -14,15 +14,16 @@ def main(args):
     analyzer = VoiceAnalyzer(api_key=api_key)
     stt_pipe = STTPipe()
     ''' load rttm file '''
-    rttm_file = local_file_path.replace('/audio/', '/rttm/').replace('.wav', '.rttm')
-    rttm_result = stt_pipe.read_rttm(rttm_file)
+    # rttm_file = local_file_path.replace('/audio/', '/rttm/').replace('.wav', '.rttm')
+    # rttm_result = stt_pipe.read_rttm(rttm_file)
     ''' analyze with diar info '''
-    processed_rttm = analyzer.process_rttm_for_stt(rttm_result)
-    result = analyzer.analyze_with_timeline(file_path=va_file_path, input_type=args.input_type, timeline=processed_rttm)
-    processd_segments = analyzer.process_stt_result(result)
-    
-    save_file_name = local_file_path.split('/')[-1].split('.')[0] + '_va.json'   # va: voice analyzer
-    analyzer.save_result_to_json(processd_segments=processd_segments, save_path=os.path.join('./dataset/stt/', save_file_name))
+    # processed_rttm = analyzer.process_rttm_for_stt(rttm_result)
+    # result = analyzer.analyze_with_timeline(file_path=va_file_path, input_type=args.input_type, timeline=processed_rttm)
+    result = analyzer.analyze(file_path=va_file_path, input_type=args.input_type)
+    # processd_segments = analyzer.process_stt_result(result)
+    print(result)
+    # save_file_name = local_file_path.split('/')[-1].split('.')[0] + '_va.json'   # va: voice analyzer
+    # analyzer.save_result_to_json(processd_segments=processd_segments, save_path=os.path.join('./dataset/stt/', save_file_name))
 
 if __name__ == "__main__":
     cli_parser = argparse.ArgumentParser(description="Voice Analyzer Test")
